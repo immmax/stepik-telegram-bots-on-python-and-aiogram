41e101fc5c355da42f18aa1e085711137be5d8e0
Maxim Datskiy <datskiumax@gmail.com>
2023-07-31 12:22:31 -0500

    Рефакторинг кода.

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from config.config import BOT_TOKEN
+from config.token import BOT_TOKEN
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
@@ -7,21 +7,25 @@ from aiogram.types import Message
 bot: Bot = Bot(token=BOT_TOKEN)
 dp: Dispatcher = Dispatcher()
 
+
 # "/start" command handler
 async def process_start_command(message: Message):
     await message.answer("Hello!\nI'm Echo Bot.\nText me something.")
+
 
 # "/help" command handler
 async def process_help_command(message: Message):
     await message.answer("Text me something. As an answer "
                          "I'll send you your message.")
 
+
 # other commands handler
 async def send_echo(message: Message):
     try:
         await message.reply(text=message.text)
     except Exception as e:
-        await message.reply(text=f"Я пока не могу обрабатывать такие сообщения:\n{e}")
+        await message.reply(text=f"Я пока не могу обрабатывать "
+                                 f"такие сообщения:\n{e}")
 
 dp.message.register(process_start_command, Command(commands=["start"]))
 dp.message.register(process_help_command, Command(commands=["help"]))
--- config/config_template.py
+++
@@ -1,2 +0,0 @@
-API_URL: str = "https://api.telegram.org/bot"
-BOT_TOKEN: str = "" # В кавычки необходимо добавить токен Вашего бота
