6606533cd3840708abafdfe5a99b5007cc96b003
Maxim Datskiy <datskiumax@gmail.com>
2023-07-31 16:52:38 -0500

    Изменил способ регистрации handler'ов - было через метод диспетчера, стало через декораторы.

--- bot.py
+++ bot.py
@@ -11,17 +11,20 @@ dp: Dispatcher = Dispatcher()
 
 
 # "/start" command handler
+@dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     await message.answer("Hello!\nI'm Echo Bot.\nText me something.")
 
 
 # "/help" command handler
+@dp.message(Command(commands=["help"]))
 async def process_help_command(message: Message):
     await message.answer("Text me something. As an answer "
                          "I'll send you your message.")
 
 
 # other commands handler
+@dp.message()
 async def send_echo(message: Message):
     try:
         await bot(message.send_copy(chat_id=message.chat.id))
@@ -30,9 +33,6 @@ async def send_echo(message: Message):
         await message.reply(text=f"Данный тип апдейтов не поддерживается "
                                  f"методом send_copy:\n{e}")
 
-dp.message.register(process_start_command, Command(commands=["start"]))
-dp.message.register(process_help_command, Command(commands=["help"]))
-dp.message.register(send_echo)
 
 if __name__ == "__main__":
     dp.run_polling(bot)
