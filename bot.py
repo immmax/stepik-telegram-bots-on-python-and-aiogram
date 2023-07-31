b76a356ead8fb52a2781c473b4f9001d6bd4a6c1
Maxim Datskiy <datskiumax@gmail.com>
2023-07-31 02:06:25 -0500

    Регистрация обработчиков через диспетчер. Добавлена обработка исключения для не текстовых сообщений.

--- bot.py
+++ bot.py
@@ -8,20 +8,24 @@ bot: Bot = Bot(token=BOT_TOKEN)
 dp: Dispatcher = Dispatcher()
 
 # "/start" command handler
-@dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     await message.answer("Hello!\nI'm Echo Bot.\nText me something.")
 
 # "/help" command handler
-@dp.message(Command(commands=["help"]))
 async def process_help_command(message: Message):
     await message.answer("Text me something. As an answer "
                          "I'll send you your message.")
 
 # other commands handler
-@dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    try:
+        await message.reply(text=message.text)
+    except Exception as e:
+        await message.reply(text=f"Я пока не могу обрабатывать такие сообщения:\n{e}")
+
+dp.message.register(process_start_command, Command(commands=["start"]))
+dp.message.register(process_help_command, Command(commands=["help"]))
+dp.message.register(send_echo)
 
 if __name__ == "__main__":
     dp.run_polling(bot)
