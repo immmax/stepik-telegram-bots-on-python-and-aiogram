ca131e97dbfc6f26e19fd573c0fda9143c5f4fc3
Maxim Datskiy <datskiumax@gmail.com>
2023-08-08 19:28:51 -0500

    Добавил user_handlers.py

---
+++ b5_modular_echo_bot/handlers/user_handlers.py
@@ -0,0 +1,11 @@
+from aiogram.types import Message
+from aiogram.filters import Command, CommandStart
+from lexicon.lexicon import LEXICON_RU
+
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text=LEXICON_RU['/start'])
+
+@dp.message(Command(commands='help'))
+async def process_celp_command(message: Message):
+    await message.answer(text=LEXICON_RU['/help'])
