5eb6e46cbcd8c27bbcd7bc3cf4153543e63610b4
Maxim Datskiy <datskiumax@gmail.com>
2023-08-10 18:54:35 -0500

    Add Menu button

--- b7_rock_scissors_paper_bot/bot.py
+++ b7_rock_scissors_paper_bot/bot.py
@@ -4,8 +4,11 @@ import logging
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
 from handlers import user_handlers, other_handlers
+from keyboards.set_menu import set_main_menu
 
 logger = logging.getLogger(__name__)
+
+
 
 async def main():
 
@@ -25,6 +28,7 @@ async def main():
     dp.include_router(user_handlers.router)
     dp.include_router(other_handlers.router)
 
+    await set_main_menu(bot=bot)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
